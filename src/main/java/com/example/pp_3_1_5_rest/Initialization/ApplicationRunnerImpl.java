53463760709d3370fb1cdda4980d4df02adb1996
ViktorTimkin <113607914+ViktorTimkin@users.noreply.github.com>
2023-01-23 20:43:52 +0400

    commit

--- src/main/java/com/example/pp_3_1_5_rest/Initialization/ApplicationRunnerImpl.java
+++ src/main/java/com/example/pp_3_1_5_rest/Initialization/ApplicationRunnerImpl.java
@@ -5,17 +5,15 @@ import com.example.pp_3_1_5_rest.models.User;
 import com.example.pp_3_1_5_rest.service.RoleService;
 import com.example.pp_3_1_5_rest.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 
+import javax.annotation.PostConstruct;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Component
-public class ApplicationRunnerImpl implements ApplicationRunner {
+public class ApplicationRunnerImpl {
     private final UserService userService;
     private final RoleService roleService;
     @Autowired
@@ -25,7 +23,21 @@ public class ApplicationRunnerImpl implements ApplicationRunner {
         this.roleService = roleService;
     }
 
-    @Override
+    @PostConstruct
+    public void postConstruct() {
+        roleService.addRole(new Role("ROLE_ADMIN"));
+        roleService.addRole(new Role("ROLE_USER"));
+        Role admin = roleService.getRoleById(1L);
+        Role user = roleService.getRoleById(2L);
+        Set<Role> adminRole = new HashSet<>();
+        Set<Role> userRole = new HashSet<>();
+        adminRole.add(admin);
+        userRole.add(user);
+        userService.addUser(new User("admin", ("admin"), adminRole));
+        userService.addUser(new User("user", ("user"), userRole));
+    }
+
+    /*@Override
     public void run(ApplicationArguments args) {
         List<User> users = userService.getAllUsers();
         if (users.isEmpty()) {
@@ -40,5 +52,6 @@ public class ApplicationRunnerImpl implements ApplicationRunner {
             userService.addUser(new User("admin", ("admin"), adminRole));
             userService.addUser(new User("user", ("user"), userRole));
         }
-    }
+    }*/
+
 }
